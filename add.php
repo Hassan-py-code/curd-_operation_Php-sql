d9ef2a5e51d5eaf26288b15185c23075a0b1d655
Hassan_Achehrri <hachehri@gmail.com>
2026-01-20 11:15:24 +0000

    started pojects curd  operation add 3 files index.php to see result and file add.php to write your data in form and file css to styles this

---
+++ add.php
@@ -0,0 +1,34 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add User</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     
+ 
+    <div class="wrapper">
+        <div class="form-wrappeer">
+            <h1>Add User</h1>
+            <form action="action.php" method="post">
+                <input type="text" name="name" placeholder="Name" require>
+                <input type="email" name="email" placeholder="Email" require>
+                <input type="tel" name="Phone" placeholder="Phone" require>
+                <textarea name="address" placeholder="Address" require></textarea>
+                <div class="btn-box">
+                    <button type="submit" class="btn" name="add">Submit</button>
+                    <a href="index.php" class="btn">Cancel</a>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>curd  operation using php & Mysql ! condehal</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div class="container">
+         <h1>User list</h1>
+         <a href="add.php">Add User</a>
+   
+
+    <table>
+         <tr>
+            <th>No.</th>
+            <th>Nmae</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>Actions</th>
+         </tr>
+         <tr>
+             <td>1</td>
+             <td>Codehal</td>
+             <td>code@ex.com</td>
+             <td>012345677</td>
+             <td>san francisco usa </td>
+             <td><a href="edit.php">Edit</a>
+              <a href="" class="btn-delete">Delete</a></td>
+           
+         </tr>
+    </table>
+    </div>
+
+
+</body>
+</html>
